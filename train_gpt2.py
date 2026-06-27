a0756571abe3d33ac31f3000e7b633776688dd0c
jimmyjia <jimmyjia@bu.edu>
2026-06-27 21:29:28 +0000

    Add support for loading and converting pretrained GPT-2 models from HuggingFace, update GPTConfig defaults, and adjust notebook examples

--- train_gpt2.py
+++ train_gpt2.py
@@ -2,6 +2,7 @@ import math
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
+from torch._inductor.runtime.caching import config
 from torch.nn import functional as F
 
 # ---------------------------------------------------------------
@@ -31,6 +32,7 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B,T,self.n_head,C // self.n_head).transpose(1,2) # (B, nh, T, hs)
         # attention (T,T) matrix for all the queries and keys
         att = q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1)))
+        # Apply the causal mask (predict next token model only mask)
         att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
         # Apply softmax on last dim of attention matrix
         att = F.softmax(att,dim=-1)
@@ -71,13 +73,14 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+    block_size: int = 1024  # context window size
+    vocab_size: int = 50257 # number of tokens: 50000 BPE merges + 256 bytes tokens + 1 EOS token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of attention heads
+    n_embd: int = 768 # embedding dimension
 
 class GPT(nn.Module):
+
     def __init__(self, config):
         super().__init__()
         self.config = config
@@ -89,3 +92,53 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd), # final layer norm
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # final, unnormalized logits layer
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """
+        Load a pretrained GPT-2 model weights from HuggingFace.
+        """
+        assert model_type in {'gpt2','gpt2-medium','gpt2-large','gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" %model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768),
+            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024),
+            'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280),
+            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+        # create from-zero init miniGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(GPTConfig)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard mask
+
+        # init a hf/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # Copy while ensuring all of the parameters are aligned and match name and shape
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a difference in parameter naming
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same
+        transposed = ['attn_c_attn.weight', 'attn_c_proj.weight', 'mlp_c_fc.weight', 'mlp_c_proj.weight']
+
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf(k).t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+
+        return model
+
